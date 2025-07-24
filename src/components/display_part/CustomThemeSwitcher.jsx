de53ed0ec2a0d70c4094b6323ad55823d94db5cd
DingoPham <pencibambo@gmail.com>
2025-07-24 15:22:06 +0700

    dd

--- src/components/display_part/CustomThemeSwitcher.jsx
+++ src/components/display_part/CustomThemeSwitcher.jsx
@@ -1,27 +1,20 @@
-import React from "react";
+import React,{useState} from "react";
 import ThemeContext from "../context/ThemeContext"; 
 
-class CustomThemeSwitcher extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      theme: 'light',
-    };
-  }
+function CustomThemeSwitcher({children}){
+  const [theme, setTheme] = useState('theme')
 
-  toggleTheme = () => {
-    this.setState((prevState) => ({
+  const toggleTheme = () => {
+    setTheme((prevState) => ({
       theme: prevState.theme === 'light' ? 'dark' : 'light',
     }));
   };
 
-  render() {
-    return (
-        <ThemeContext.Provider value={{theme: this.state.theme, toggleTheme: this.toggleTheme}}>
-            {this.props.children}
-        </ThemeContext.Provider>
-    );
-  }
+  return (
+    <ThemeContext.Provider value={{theme: theme, toggleTheme: toggleTheme}}>
+        {children}
+    </ThemeContext.Provider>
+  );
 }
 
 export default CustomThemeSwitcher;
